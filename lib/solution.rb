d28c2d35f957e01d1517af5382eb38775d6b5a28
JazzyMussels <jeffmussels@gmail.com>
2019-09-10 10:11:40 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,11 @@
 def clock_angle(time)
-    # code your solution here
+    nums = time.split(':').map(&:to_i)
+    hr = nums[0].to_f
+    min = nums[1].to_f
+    (min == 0 && hr > 6 && hr < 12) ? min = 360.0 : min
+
+    degrees_per_hr = hr * 30.0
+    mins_per_hr = (min / 2.0) - (min * 6.0)
+    mod = (degrees_per_hr + mins_per_hr) % 360
+    mod.abs
 end
