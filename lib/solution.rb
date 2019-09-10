9f299edddd7ec7bca16f6726a06632c9f8c12062
jw20191n <clairejw1015@gmail.com>
2019-09-10 10:05:19 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -2,8 +2,8 @@ def clock_angle(time)
     # code your solution here
     time = time.split(":")
 
-    minute = 6
-    hr_min = 0.5
+    # minute = 6
+    # hr_min = 0.5
 
     angle = ((time[0].to_i * 60 + time[1].to_i) * 0.5 - time[1].to_i * 6).abs
     
