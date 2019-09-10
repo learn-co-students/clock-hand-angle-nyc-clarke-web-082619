6b7091033a50979b8bd3ecfb2068c19da8e8ddc7
github username <github email address>
2019-09-10 09:57:50 -0400

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,18 @@
 def clock_angle(time)
-    # code your solution here
+    time_array = time.split(":")
+    hour = time_array[0].to_f
+    minute = time_array[1].to_f
+
+  #  total_minutes = (hour * 60) + minute
+
+    m_hand_pos = (minute * 6)
+    h_hand_pos = ((hour * 30)+ (m_hand_pos/12))
+
+    total = (m_hand_pos - h_hand_pos).abs
+
+    if total >= 180 
+        360 - total 
+    else
+        total
+    end
 end
